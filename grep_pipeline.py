2ca276a8b37e57add6d413af7dd1c518143264dd
redvg <redvg17@gmail.com>
2018-08-06 14:06:35 +0200

    adds grep pipeline

---
+++ grep_pipeline.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+import apache_beam as beam
+import re
+import sys
+
+def custom_grep(line, search_query):
+
+   if re.match( r'^' + re.escape(search_query), line):
+
+      yield line
+
+if __name__ == '__main__':
+
+   pipeline = beam.Pipeline(argv=sys.argv)
+
+   input = '../javahelp/src/main/java/com/google/cloud/training/dataanalyst/javahelp/*.java'
+
+   output_prefix = '/tmp/grep_pipeline_output'
+
+   search_query = 'import'
+
+   (pipeline
+      | 'GetJava' >> beam.io.ReadFromText(input)
+      | 'Grep' >> beam.FlatMap(lambda line: custom_grep(line, search_query))
+      | 'write' >> beam.io.WriteToText(output_prefix)
+   )
+
+   pipeline.run().wait_until_finish()

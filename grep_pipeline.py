e0a17df3dad61f47483c7b0630620dd9f4cbfd40
redvg <redvg17@gmail.com>
2018-08-06 14:45:02 +0200

    fixes path

--- grep_pipeline.py
+++ grep_pipeline.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
    pipeline = beam.Pipeline(argv=sys.argv)
 
-   input = '/java/*.java'
+   input = './java/*.java'
 
    output_prefix = '/tmp/grep_pipeline_output'
 
